1a80fc603ede5d565d8d4abae96ca32321a0836c
Knhash <Mr.Knhash@gmail.com>
2017-05-06 02:06:27 +0530

    feat: Wire Cube, spinning, colored

--- PrettyPixel.cpp
+++ PrettyPixel.cpp
@@ -1,47 +1,61 @@
 #include <iostream>
+#include <stdlib.h>
 #include <GL/glut.h>
 
 //Global defaults
 int screenWidth = 661;
 int screenHeight = 661;
+float xAngle=0, yAngle=0, zAngle=0;
+int timerDuration = 10;
+float randNum;
 
 using namespace std;
 
-void Initialize() {
-	glClearColor(0.0, 0.0, 0.0, 0.0);//Select clear color as black
-
-	//Initialize viewing aspects
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
-}
-
 void Pretty() {
 	glClear(GL_COLOR_BUFFER_BIT);		//Clear all pixels
+	glColor3f((rand() % 100)/100.0, (rand() % 100)/100.0, (rand() % 100)/100.0);		//Random colors
+	glLoadIdentity();
+	gluLookAt (3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);		//eyex, eyey, eyez, centrex, centrey, centrez, upx, upy, upz
+	glRotatef(xAngle, 1.0f, 0.0f, 0.0f);
+	glRotatef(yAngle, 0.0f, 1.0, 0.0f);
+	glRotatef(zAngle, 0.0f, 0.0f, 1.0f);
+	glLineWidth(3);
+	glutWireCube (3.0); 
+	glFlush();
 
-	glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_POLYGON);		//Draw square
-        glVertex3f(0.25, 0.25, 0.0);
-        glVertex3f(0.75, 0.25, 0.0);
-        glVertex3f(0.75, 0.75, 0.0);
-        glVertex3f(0.25, 0.75, 0.0);
-    glEnd();
+    glFlush();
+}
 
-    glFlush();		//Render immediately
+void Reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.5);		//3D projection
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void Timer(int v) {
+	xAngle=yAngle=zAngle+=1;
+	if(xAngle>360)
+		xAngle=yAngle=zAngle-=360;
+	glutPostRedisplay();
+	glutTimerFunc(timerDuration, Timer, 0);
 }
 
 int main (int argc, char **argv) {
-	cout << "Hello World!" << endl;
+	cout << "Hello World! Pretty Pixel\n";
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-screenWidth)/2, (glutGet(GLUT_SCREEN_HEIGHT)-screenHeight)/2);		//Display window at screen centre
 	glutInitWindowSize(screenWidth, screenHeight);
 	glutCreateWindow("Pretty Pixel");
-
-	Initialize();
+	glClearColor(0.0, 0.0, 0.0, 0.0);		//Select clear color as black
+	glShadeModel(GL_FLAT);
 
 	glutDisplayFunc(Pretty);		//Display event callback
+	glutReshapeFunc(Reshape);		//Display window reshape event callback
+	glutTimerFunc(timerDuration, Timer, 0);		//Timer for infinite update and display
 	glutMainLoop();
 
 	return 0;
