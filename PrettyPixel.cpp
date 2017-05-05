b59ffff205c3c26f3728032a03bdfcf83f317a9b
Knhash <Mr.Knhash@gmail.com>
2017-05-05 20:14:42 +0530

    start: Hello World!

---
+++ PrettyPixel.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <GL/glut.h>
+
+using namespace std;
+
+int main (int argc, char **argv) {
+	cout << "Hello World!" << endl;
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Pretty-Pixel
+Use your mouse to rotate the playing field. Your goal is to find a viewpoint where pixels form a picture within minimum time.
+
