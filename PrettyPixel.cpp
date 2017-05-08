ff767f54eb38a560bf41860171cd149a177c6d72
Knhash <Mr.Knhash@gmail.com>
2017-05-08 23:21:18 +0530

    fix: Remove MouseMove.txt

--- PrettyPixel.cpp
+++ PrettyPixel.cpp
@@ -6,45 +6,37 @@
 //Global defaults
 int screenWidth = 800;
 int screenHeight = 800;
-float mouseX = 0.0, mouseY = 0.0;
+float mouseX = 0.0, mouseY = 0.0, curX = 0.0, curY = 0.0;
 float 
 	cx = 0.0, cy = 0.0, cz = 5.0,
 	lx = 0.0, ly = 0.0, lz = 0.0;
 int timerDuration = 50;
 float fraction = 0.0;
+bool LMB = false;
 
 using namespace std;
 
-void DrawPixels() {
-	//glutWireCube(700);
-	//glutWireTeapot(1);
-	glLineStipple(1, 0x00FF);
-	glBegin(GL_LINE_LOOP);
-	glVertex3f( -50*2,  -50*2, -20*2);
-		glVertex3f( -50*2,  -40*2, -50*2);
-		glVertex3f( -50*2,  -20*2, -30*2);
-		glVertex3f( -50*2,  30*2, 30*2);
-	glVertex3f( -50*2,  50*2, 10*2);
-		glVertex3f( -20*2,  50*2, -10*2);
-		glVertex3f( 30*2,  50*2, 20*2);
-		glVertex3f( 40*2,  50*2, -20*2);
-	glVertex3f( 50*2,  50*2, 0*2);
-		glVertex3f( 50*2,  20*2, 10*2);
-		glVertex3f( 50*2,  -30*2, 20*2);
-		glVertex3f( 50*2,  -40*2, 30*2);
-	glVertex3f( 50*2,  -50*2, 10*2);
-		glVertex3f( 30*2,  -50*2, -20*2);
-		glVertex3f( 10*2,  -50*2, 30*2);
-		glVertex3f( -35*2,  -50*2, -40*2);
+void DisplayPixels() {
+	//glutWireCube(400);
+	glutWireTeapot(300);
+	
+}
+
+void DrawPixel(int x, int y) {
+	glPointSize(10);
+	glBegin(GL_POINTS);
+	glColor3f(((rand() % 100) / 100.0), ((rand() % 100) / 100.0), ((rand() % 100) / 100.0));
+	glVertex3f(x, y, 0);
 	glEnd();
 }
 
 void Pretty() {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);	
 	glLoadIdentity();
 	gluLookAt (cx, cy, cz, lx, ly, lz, 0.0, 1.0, 0.0);
-	DrawPixels();
+	DisplayPixels();
+	//DrawPixel(0,0);
 	glFlush();
 }
 
@@ -57,57 +49,44 @@ void Reshape(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
+void MouseClickMove(int x, int y) {
+	curX = x - (screenWidth / 2), curY = (screenHeight - y) - (screenHeight / 2);
+	if(LMB == true) {
+		cout << "Click at (" << curX << "," <<  curY << ")\n";
+		DrawPixel(curX, curY);
+		glFlush();
+	}	
+}
+
 void MouseMove(int x, int y) {
-	cout << "Mouse at (" << x << "," << y - screenHeight << ")\n";
-	if(abs(x - mouseX/100.0) > 0) {
-		mouseX = x/100.0;
-		mouseY = (y - screenHeight)/100.0;
+	curX = x - (screenWidth / 2), curY = (screenHeight - y) - (screenHeight / 2);
+	cout << "Mouse at (" << curX << "," <<  curY << ")\n";
+	if(abs(curX - mouseX) > 0) {
+		mouseX = curX/(200);
+		mouseY = curY/(200);
 		cx = sin(mouseX)*10;
 		cz = cos(mouseX)*10;
 		glutPostRedisplay();
 	}
-	if(abs((y - screenHeight) - mouseY/100.0) > 0) {
-		mouseX = x/100.0;
-		mouseY = (y - screenHeight)/100.0;
+	if(abs(curY - mouseY) > 0) {
+		mouseX = curX/(screenWidth/2);
+		mouseY = curY/(screenHeight/2);
 		cy = sin(mouseY)*10;
 		cz = cos(mouseY)*10;
 		glutPostRedisplay();
 	}
 }
 
-void KeyArrow(int key, int x, int y) {
-	switch(key) {
-		case GLUT_KEY_LEFT :
-			mouseX += 0.1;
-			cx = sin(mouseX)*10;
-			cz = cos(mouseX)*10;
-			glutPostRedisplay();
-			break;
-		case GLUT_KEY_RIGHT :
-			mouseX -= 0.1;
-			cx = sin(mouseX)*10;
-			cz = cos(mouseX)*10;
-			glutPostRedisplay();
-			break;
-		case GLUT_KEY_UP:
-			mouseY += 0.1;
-			cy = sin(mouseY)*10;
-			cz = cos(mouseY)*10;
-			glutPostRedisplay();
-			break;
-		case GLUT_KEY_DOWN :
-			mouseY -= 0.1;
-			cy = sin(mouseY)*10;
-			cz = cos(mouseY)*10;
-			glutPostRedisplay();
-		case GLUT_KEY_PAGE_UP:
-			cz += 0.1;
-			glutPostRedisplay();
-			break;
-		case GLUT_KEY_PAGE_DOWN :
-			cz -= 0.1;
-			glutPostRedisplay();
-			break;
+void MouseClick(int button, int state, int x, int y) {
+	curX = x - (screenWidth / 2), curY = (screenHeight - y) - (screenHeight / 2);
+	switch(button) {
+		case GLUT_LEFT_BUTTON :
+			if(state == GLUT_DOWN) {
+				LMB = true;
+			}
+			else if(state == GLUT_UP) {
+				LMB = false;
+			}
 	}
 }
 
@@ -124,7 +103,7 @@ int main (int argc, char **argv) {
 	cout << "Hello World! Pretty Pixel\n";
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
 	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-screenWidth)/2, 
 							(glutGet(GLUT_SCREEN_HEIGHT)-screenHeight)/2);
 	glutInitWindowSize(screenWidth, screenHeight);
@@ -136,7 +115,9 @@ int main (int argc, char **argv) {
 	glutDisplayFunc(Pretty);		
 	glutReshapeFunc(Reshape);
 	//glutTimerFunc(timerDuration, Timer, 0);
-	//glutPassiveMotionFunc(MouseMove);
+	glutMotionFunc(MouseClickMove);
+	glutMouseFunc(MouseClick);
+	glutPassiveMotionFunc(MouseMove);
 	//glutSpecialFunc(KeyArrow);
 	glutMainLoop();
 
--- ppRun.sh
+++
@@ -1,2 +0,0 @@
-#Compile and Run Pretty Pixel
-g++ PrettyPixel.cpp -o OutPrettyPixel -lGL -lGLU -lglut && ./OutPrettyPixel
